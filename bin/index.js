fd03ea39ef66be50d906914326a5391f7ce405ca
Giacomo Zinetti <giacomo.zinetti@gmail.com>
2019-06-03 17:00:41 +0200

    big update

--- bin/index.js
+++ bin/index.js
@@ -1,57 +1,26 @@
 #!/usr/bin/env node
-const fs = require("fs");
-const path = require("path");
-const pkgDir = require("pkg-dir");
-const { spawn } = require("child_process");
-var inquirer = require("inquirer");
+const installer = require('../lib/installer.js')
 
-const installers = {
-  npm: () => {
-    spawn("npm", ["install"], { stdio: "inherit" });
+installer([
+  {
+    name: 'npm',
+    cli: 'npm',
+    args: ['install'],
+    lockFile: 'package-lock.json',
+    installer: 'npm install npm@latest -g'
   },
-  yarn: () => {
-    spawn("yarn", [], { stdio: "inherit" });
+  {
+    name: 'yarn',
+    cli: 'yarn',
+    args: ['install'],
+    lockFile: 'yarn.lock',
+    installer: 'curl -o- -L https://yarnpkg.com/install.sh | bash'
+  },
+  {
+    name: 'pnpm',
+    cli: 'pnpm',
+    args: ['install'],
+    lockFile: 'pnpm-lock.yaml',
+    installer: 'curl -L https://unpkg.com/@pnpm/self-installer | node'
   }
-};
-
-function choose(list) {
-  return inquirer
-    .prompt([
-      {
-        type: "list",
-        name: "tech",
-        message: "What package manager do you want to use?",
-        choices: list
-      }
-    ])
-    .then(answers => {
-      installers[answers.tech]();
-    });
-}
-
-const rootDir = pkgDir.sync();
-let installer = [];
-
-try {
-  fs.accessSync(path.join(rootDir, "package-lock.json"));
-  installer.push("npm");
-} catch (err) {
-  // console.error("no access!");
-}
-
-try {
-  fs.accessSync(path.join(rootDir, "yarn.lock"));
-  installer.push("yarn");
-} catch (err) {
-  // console.error("no access!");
-}
-
-if (installer.length === 0) {
-  console.log("😢 No lock file found");
-  choose(["npm", "yarn"]);
-} else if (installer.length === 1) {
-  installers[installer]();
-} else {
-  console.log("🔥 More lock files found");
-  choose(installer);
-}
+])
